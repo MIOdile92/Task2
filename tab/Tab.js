a1da1d1d918c2aa8cdfd878932130c9516a90632
MIOdile92 <819967089@qq.com>
2016-12-03 21:40:29 +0800

    Task2

--- tab/Tab.js
+++ tab/Tab.js
@@ -1,21 +1,21 @@
 
 window.onload = function () {
-	"use strict";
-	var tab_menu = document.getElementsById("tab_menu");
+	var tab_menu = document.getElementById("tab_menu");
 	var tab_menu_li = tab_menu.getElementsByTagName("li");
-	var message = document.getElementsById("message");
+	var message = document.getElementById("message");
 	var message_li = message.getElementsByTagName("div");
 	var len = tab_menu_li.length;
 
-	for (var i = 0; i<length; i++) {
+	for (var i = 0; i<len; i++) {
 		tab_menu_li[i].index = i;
 		tab_menu_li[i].onclick = function(){
-			for (var i = 0; i < len; i++) {
-				tab_menu_li[i].className = '';
-				tab_menu_li[i].className = 'hide';
+			for (var j = 0; j < len; j++) {
+				tab_menu_li[j].className = '';
+				message_li[j].style.display= 'none';
 			}
-			tab_menu_li[this.index].className = 'message';
-			message_li[this.index].className = '';
-		}
+			tab_menu_li[this.index].className = 'selected';
+			tab_menu_li[this.index].className = 'selected';
+			message_li[this.index].style.display = 'block';
+		};
 	}
-}
+};
